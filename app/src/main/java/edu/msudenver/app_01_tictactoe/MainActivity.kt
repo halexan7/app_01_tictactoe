62f4a4175a6f63c8bdd9c270a72321609d10027f
halexan7 <halexan7@msudenver.edu>
2022-06-29 12:08:06 -0600

    3rd

--- app/src/main/java/edu/msudenver/app_01_tictactoe/MainActivity.kt
+++ app/src/main/java/edu/msudenver/app_01_tictactoe/MainActivity.kt
@@ -11,14 +11,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioGroup
+import android.widget.*
 import edu.msudenver.app_01_tictactoe.R
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var playButton: Button
+    private lateinit var playerName: EditText
+    private lateinit var noughts: RadioButton
+    private lateinit var crosses: RadioButton
+    private lateinit var first: RadioButton
+    private lateinit var notFirst: RadioButton
+    private lateinit var playerInfo: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,16 +30,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         // TODO (suggested): get a reference to the "play" button and use it to set its "onClick" listener to MainActivity
         playButton = findViewById(R.id.playButton)
-        playButton.setOnClickListener(){
-
-        }
-
+        playButton.setOnClickListener(this)
     }
 
     // TODO (suggested): get the player's name, symbol, and the first move choice; pass info to the TicTacToe activity
     override fun onClick(p0: View?) {
+        playerName = findViewById(R.id.playerName)
+        val name = playerName.text.toString()
 
+        noughts = findViewById(R.id.noughts)
+        crosses = findViewById(R.id.crosses)
+        val symbol = if (noughts.isChecked) 'O' else 'X'
+
+        first = findViewById(R.id.first)
+        notFirst = findViewById(R.id.notFirst)
+        val firstMove = first.isChecked
+
+        val intent = Intent(this, TicTacToeActivity::class.java)
+        intent.putExtra("name", name)
+        intent.putExtra("symbol", symbol)
+        intent.putExtra("first move", firstMove)
+        startActivity(intent)
     }
-
-
 }
--- app/src/main/java/edu/msudenver/app_01_tictactoe/TicTacToeActivity.kt
+++ app/src/main/java/edu/msudenver/app_01_tictactoe/TicTacToeActivity.kt
@@ -15,40 +15,38 @@ import androidx.appcompat.app.AppCompatActivity
 import edu.msudenver.app_01_tictactoe.R
 
 class TicTacToeActivity : AppCompatActivity(),View.OnClickListener {
-    private lateinit var playerName: EditText
-    private lateinit var noughts: RadioButton
-    private lateinit var crosses: RadioButton
-    private lateinit var first: RadioButton
-    private lateinit var notFirst: RadioButton
     private lateinit var playerInfo: TextView
+    private lateinit var gridLayout: GridLayout
 
 
 
     // TODO (suggested): maintain a reference to a TicTacToe object
-    val game = TicTacToe
+    val game: TicTacToe ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tictactoe)
 
         // TODO (suggested): get the player's name, symbol, and the first move choice from the activity's intent; use them to instantiate a TicTacToe game; decide whether the computer should play based on the first move choice
-        playerName = findViewById(R.id.playerName)
-        val name = playerName.toString()
-
-        noughts = findViewById(R.id.noughts)
-        crosses = findViewById(R.id.crosses)
-        val symbol = if (noughts.isChecked) "O" else "X"
-
-        first = findViewById(R.id.first)
-        notFirst = findViewById(R.id.notFirst)
-        val firstMove = first.isChecked
+        val playerName = intent.getStringExtra("name")
+        val symbol = intent.getCharExtra("symbol",'?')
+        val firstMove = intent.getBooleanExtra("first move",false)
 
         // TODO (suggested): get a reference to the TextView "player info" area; update the TextView with the player's name and symbol
         playerInfo = findViewById(R.id.playerInfo)
         playerInfo.text = "Player $playerName is using $symbol"
 
+
         // TODO (suggested): using a loop and button tags, update their texts and "onClick" listeners to TicTacToeActivity; remember to disable the button if it corresponds to a computer's first move
         // hint: use "findViewWithTag"
+        gridLayout = findViewById(R.id.gridLayout)
+        for (i in 0 until 3){
+            for (j in 0 until 3){
+                val tag: String = "$i,$j"
+                val button: Button = gridLayout.findViewWithTag(tag)
+                button.setOnClickListener(this)
+            }
+        }
 
     }
 
@@ -58,6 +56,12 @@ class TicTacToeActivity : AppCompatActivity(),View.OnClickListener {
 
     // TODO (suggested): cast the given view as a Button; disable the button so you don't forget; get the button's tag and use it to infer the player's move coordinates; make the move and update the button's text with the player's symbol; if the game is over, show results; otherwise, have the computer play; use TitTacToe's last move and "findViewWithTag" to get a reference to the button of the computer's play; disable the button so you don't forget; update the button's text with the computer's symbol; if the game is over, show results
     override fun onClick(view: View?) {
+        val button = view as Button
+        button.isEnabled = false
+        val tag = button.tag
+        game?.playerPlay(tag as Int)
+        button.text = intent.getCharExtra("symbol",'?').toString()
+
 
     }
 
